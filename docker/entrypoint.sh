ef9669460d856e7d821fe7ede6a981d3ea55fce1
David Eze <davideze7689@gmail.com>
2026-08-03 18:34:53 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Source ROS 2 and the workspace if it has been built.
 source /opt/ros/jazzy/setup.bash
 if [ -f /workspace/install/setup.bash ]; then
     source /workspace/install/setup.bash
